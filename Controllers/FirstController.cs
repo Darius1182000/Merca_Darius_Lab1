a9f6da0335d08991757caae588ce9d502a2a70c8
Darius1182000 <merca_darius@yahoo.ro>
2022-10-11 22:11:36 +0300

    Am adaugat 3 metode diferite si un controller.

---
+++ Controllers/FirstController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Merca_Darius_Lab1.Controllers
+{
+    public class FirstController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Welcome");
+        }
+
+        public IActionResult Salut()
+        {
+            return Content("Bune venit draga utilizator!");
+        }
+
+        public IActionResult Varsta(string nume, int ani)
+        {
+            return Content("Persoana cu numele " + nume + " are " + ani + " ani!");
+        }
+    }
+}
